5ef3842134251c8c32e864e75ffa0faba2caa7f5
Adidsus <85895319+Adidsus@users.noreply.github.com>
2024-12-10 11:44:55 +0100

    Update ballsinpocket.lua

--- ballsinpocket.lua
+++ ballsinpocket.lua
@@ -191,7 +191,8 @@ workspace.ChildAdded:Connect(function(child)
 end)
 
 
-elseif placeId == lobby then
+--elseif placeId == lobby then
+else  
 local Signals = {"Activated", "MouseButton1Down", "MouseButton2Down", "MouseButton1Click", "MouseButton2Click"}
 local player = game.Players.LocalPlayer
 local lobbyList = player.PlayerGui.MainGui.LobbyListFrame.Content.LobbyList
